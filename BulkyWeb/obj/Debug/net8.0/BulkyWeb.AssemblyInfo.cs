8b20e5453cb9feb5e2aa56205586179ab0fda922
Ammarhussain50 <Hussainammar205@gmail.com>
2026-01-26 16:35:09 +0500

    Order Summary UI

--- BulkyWeb/obj/Debug/net8.0/BulkyWeb.AssemblyInfo.cs
+++ BulkyWeb/obj/Debug/net8.0/BulkyWeb.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BulkyWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+97180bd3fc90e736221a295ac91b1ee70960b420")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a68af92ccc1ec83accb960290f9f27ba1ba8fbbb")]
 [assembly: System.Reflection.AssemblyProductAttribute("BulkyWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BulkyWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
