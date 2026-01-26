b2f14357c890a5dd25c66af0f041451228be1c0e
Ammarhussain50 <Hussainammar205@gmail.com>
2026-01-27 00:46:37 +0500

    summary action method

--- BulkyWeb/obj/Debug/net8.0/BulkyWeb.AssemblyInfo.cs
+++ BulkyWeb/obj/Debug/net8.0/BulkyWeb.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BulkyWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0f5daa95ca1abb419113f1847348600d40edc840")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+020457dbd5c1c8bd17ca102f2c45b8b4433bce37")]
 [assembly: System.Reflection.AssemblyProductAttribute("BulkyWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BulkyWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
