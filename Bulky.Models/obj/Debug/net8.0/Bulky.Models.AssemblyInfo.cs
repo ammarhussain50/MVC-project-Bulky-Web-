647fe8bcd70d0e263029257dfaa65b8640b49e63
Ammarhussain50 <Hussainammar205@gmail.com>
2026-02-14 00:49:58 +0500

    ship order

--- Bulky.Models/obj/Debug/net8.0/Bulky.Models.AssemblyInfo.cs
+++ Bulky.Models/obj/Debug/net8.0/Bulky.Models.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Bulky.Models")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d806df41ac0d577c0daa354e05f8508a2814f1ff")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+79b31dddc8e4a430c2bc60c6d7a36b981ad88380")]
 [assembly: System.Reflection.AssemblyProductAttribute("Bulky.Models")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Bulky.Models")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
